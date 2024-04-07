7995d239911c69a07db4dfb96357ee42b5d46419
project.aman <158331413+projectaman2357@users.noreply.github.com>
2024-04-07 10:25:40 +0530

    Add files via upload
    Step into the world of innovative design with our HTML and CSS lamp project. This virtual lamp combines artistic flair with functional elegance, showcasing the power of web technologies. Explore the intricacies of CSS styling and HTML structure as we bring this illuminating concept to life. Perfect for aspiring web designers and enthusiasts looking to brighten their skills!"

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+let btn = document.querySelector("#btn");
+let light = document.querySelector("#light");
+
+function toggleBtn(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+}
+
